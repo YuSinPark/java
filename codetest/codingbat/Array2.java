8a95e2ef8a55503d066f48f5749ce6842cf644bc
YuSinPark <pus12369@naver.com>
2023-03-10 11:19:27 +0900

    java

---
+++ codetest/codingbat/Array2.java
@@ -0,0 +1,29 @@
+package codetest.codingbat;
+
+public class Array2 {
+	public int bigDiff(int[] nums) {
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		
+		for (int n : nums) {
+			max = (max < n) ? n : max;
+			min = (min > n) ? n : min;
+			
+//			min = Math.min(min, n);
+//		    max = Math.max(max, n);
+		}
+		
+		return max - min;
+	}
+	
+	public int[] fizzArray3(int start, int end) {
+		int[] res = new int[end - start];
+		
+		for (int i = 0; i < res.length; i++) {
+			res[i] = start + i;
+		}
+		
+		return res;
+	}
+
+}
